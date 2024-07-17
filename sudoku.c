b039b06c684e4557a375ee22e4d4efbdeefb7d9c
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-16 20:52:41 -0600

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,10 +1,16 @@
 // 
 
+// Libraries and neccesary includes
 # include <stdio.h>
 # include <stdlib.h>
 # include <math.h>
 
+// Prototipe of functions
+
+// Main
 int main(){
 
     return 0;
 }
+
+// Functions
